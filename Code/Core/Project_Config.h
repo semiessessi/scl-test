a9be6624d71771285748e39eaccae33fdaca7934
Sem Essessi <sem.essessi@redmarmosetstudios.com>
2024-08-03 23:45:12 +0100

    update scl

--- Code/Core/Project_Config.h
+++ Code/Core/Project_Config.h
@@ -6,6 +6,8 @@
 #include "Handlers.h"
 #include "Core/Parameter.h"
 
+#include <vector>
+
 static const char* const kSCLTitle = "SCL Test";
 // note, this doesnt have to be const if you want handlers to modify it
 static const bool kbSCLDisplayTitle = true;
@@ -17,5 +19,7 @@ static const SCL::Parameter kSCLParameters[] =
 	{ "testes", true, Handler3 },
 	{ "teste", false, Handler4 }
 };
+
+inline int Project_EntryPoint(const std::vector<SCL::ParameterInstance>& ) { return 0; }
 
 #endif
